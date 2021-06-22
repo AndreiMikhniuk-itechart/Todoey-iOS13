1c0629cae98e9259ae3316c1b223ce49ac1790a4
Andrei-Mikhniuk <andrewmikhniuk@gmail.com>
2021-06-22 18:07:03 +0300

    add search bar

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -1,26 +1,25 @@
  
-
-import UIKit
-import CoreData
-
-class TodoListViewController: UITableViewController{
+ 
+ import UIKit
+ import CoreData
+ 
+ class TodoListViewController: UITableViewController {
     var itemArray: [Item] = []
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    let request : NSFetchRequest<Item> = Item.fetchRequest()
     
+    @IBOutlet weak var searchBar: UISearchBar!
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         loadItems()
     }
-  
+    
     
     @IBAction func addTodoAction(_ sender: Any) {
-        
         var textField: UITextField?
-        
         let allert = UIAlertController(title: "Add todo", message: nil, preferredStyle: .alert)
-        
         let allertAction = UIAlertAction(title: "add", style: .default) { (action) in
-            
             let newItem = Item(context: self.context)
             newItem.title = textField?.text!
             newItem.isDone = false
@@ -28,17 +27,12 @@ class TodoListViewController: UITableViewController{
             self.saveItem();
             
         }
-        allert.addAction(allertAction
-        )
-        
+        allert.addAction(allertAction)
         allert.addTextField(
             configurationHandler: { allertTextField in
                 allertTextField.placeholder = "Todo"
-                textField = allertTextField
-        })
-
+                textField = allertTextField })
         present(allert, animated: true) {
-            
         }
     }
     
@@ -46,38 +40,25 @@ class TodoListViewController: UITableViewController{
         return itemArray.count
     }
     
-    // Provide a cell object for each row.
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-       let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
+        // Provide a cell object for each row.
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoItemCell", for: indexPath)
         let item = itemArray[indexPath.row]
-        let attributedText : NSMutableAttributedString =  NSMutableAttributedString(string: item.title!)
-        attributedText.addAttributes([
-                        NSAttributedString.Key.strikethroughStyle: NSUnderlineStyle.single.rawValue,
-                        NSAttributedString.Key.strikethroughColor: UIColor.lightGray,
-                        NSAttributedString.Key.font : UIFont.systemFont(ofSize: 12.0)
-                        ], range: NSMakeRange(0, attributedText.length))
         cell.textLabel!.text = item.title
-    
         cell.accessoryType =  item.isDone ?.checkmark : .none
-        
-           
-       return cell
+        return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let item = itemArray[indexPath.row]
         item.isDone = !item.isDone
-        
-        
-        tableView.reloadData()
-        tableView.deselectRow(at: indexPath, animated: true)
+        // tableView.deselectRow(at: indexPath, animated: true)
         saveItem()
     }
     
     func saveItem(){
         do {
-            
-          try  self.context.save()
+            try  self.context.save()
         } catch {
             print("Error saving context \(error)")
         }
@@ -85,14 +66,37 @@ class TodoListViewController: UITableViewController{
     }
     
     func loadItems() {
-        let request : NSFetchRequest<Item> = Item.fetchRequest()
-        do{
+        do {
             self.itemArray = try context.fetch(request)
-            
         } catch {
             print("Error loading context \(error)")
         }
+        tableView.reloadData()
     }
-}
-
-
+    
+    
+ }
+ 
+ //MARK: - Search bar methods
+ 
+ extension TodoListViewController: UISearchBarDelegate {
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        search(searchBar.text!)
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        search(searchBar.text!)
+    }
+    
+    func search(_ text: String){
+        if(!searchBar.text!.isEmpty) {
+            request.predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+            request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        } else {
+            request.predicate = nil
+            request.sortDescriptors = nil
+        }
+        loadItems()
+    }
+ }
